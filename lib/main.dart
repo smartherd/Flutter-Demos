835e101d63d2e1e4b1b6ad9464091fcdaf57afe9
Smartherd <smartherd@gmail.com>
2018-09-26 20:23:51 +0530

    Section One, Video Four: Exploring Widgets and designing UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,22 @@ import "package:flutter/material.dart";
 void main() {
 
 	runApp(
-		Center(
-			child: Text(
-				"Hello Flutter",
-				textDirection: TextDirection.ltr
-		  ),
+
+		MaterialApp(
+			title: "My Flutter App",
+			home: Scaffold(
+				appBar: AppBar(title: Text("My First App Screen"),),
+				body: Material(
+					color: Colors.lightBlueAccent,
+					child: Center(
+						child: Text(
+							"Hello Flutter",
+							textDirection: TextDirection.ltr,
+							style: TextStyle(color: Colors.white, fontSize: 40.0),
+						),
+					),
+				),
+			)
 		)
 	);
 }
