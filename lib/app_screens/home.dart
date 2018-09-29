92a59be92356aec230cc3a047206178fb9f824d9
Smartherd <smartherd@gmail.com>
2018-09-29 10:04:31 +0530

    Section Two, Video Two: Container Widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -10,6 +10,9 @@ class Home extends StatelessWidget {
 	    child: Container(
 		    alignment: Alignment.center,
 		    color: Colors.deepPurple,
+		    width: 200.0,
+		    height: 100.0,
+		    margin: EdgeInsets.only(left: 35.0, top: 50.0),
 		    child: Text("Flight", textDirection: TextDirection.ltr),
 	    )
     );
