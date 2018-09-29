1549842f3cb13e60e701503edd16f008212b028b
Smartherd <smartherd@gmail.com>
2018-09-29 11:33:43 +0530

    Section Two, Video Three: Styling Text

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -10,10 +10,17 @@ class Home extends StatelessWidget {
 	    child: Container(
 		    alignment: Alignment.center,
 		    color: Colors.deepPurple,
-//		    width: 200.0,
-//		    height: 100.0,
-//		    margin: EdgeInsets.only(left: 35.0, top: 50.0),
-		    child: Text("Flight", textDirection: TextDirection.ltr),
+		    child: Text(
+			    "Flight",
+			    textDirection: TextDirection.ltr,
+			    style: TextStyle(
+				    decoration: TextDecoration.none,
+				    fontSize: 75.0,
+				    fontFamily: 'Raleway',
+				    fontWeight: FontWeight.w700,
+				    color: Colors.white
+			    ),
+		    ),
 	    )
     );
   }
