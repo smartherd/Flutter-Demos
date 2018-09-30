553db9daf9943f8ad8cea0d2c0fb72884019ac07
Smartherd <smartherd@gmail.com>
2018-09-30 08:46:00 +0530

    Section Two, Video Six: Raised Button and Alert Dialog

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -62,18 +62,59 @@ class Home extends StatelessWidget {
                     )),
                   ],
                 ),
-	              FlightImageAsset()
+                FlightImageAsset(),
+                FlightBookButton()
               ],
             )));
   }
 }
 
 class FlightImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/flight.png');
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class FlightBookButton extends StatelessWidget {
 
 	@override
   Widget build(BuildContext context) {
-    AssetImage assetImage = AssetImage('images/flight.png');
-    Image image = Image(image: assetImage, width: 250.0, height: 250.0,);
-    return Container(child: image,);
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 50.0,
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text(
+            "Book Your Flight",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700),
+          ),
+          elevation: 6.0,
+          onPressed: () => bookFlight(context)),
+    );
+  }
+
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Booked Successfully"),
+      content: Text("Have a pleasant flight"),
+    );
+
+    showDialog(
+        context: context,
+		    builder: (BuildContext context) => alertDialog);
   }
 }
