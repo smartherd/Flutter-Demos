bba338063c065cca1a4fe57d53e137ed7921eb01
Smartherd <smartherd@gmail.com>
2018-10-01 15:27:16 +0530

    Section Two, Video Eight: Long List View

--- lib/main.dart
+++ lib/main.dart
@@ -3,45 +3,39 @@ import './app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Scaffold(
-      appBar: AppBar(title: Text("Basic List View"),),
-      body: getListView(),
+
+	  title: "Exploring UI widgets",
+
+	  home: Scaffold(
+      appBar: AppBar(title: Text("Long List"),),
+      body: getListView()
     ),
+
   ));
+}
+
+List<String> getListElements() {
+
+	var items = List<String>.generate(1000, (counter) => "Item $counter");
+	return items;
 }
 
 Widget getListView() {
 
-  var listView = ListView(
-    children: <Widget>[
+	var listItems = getListElements();
 
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () {
-          debugPrint("Landscape tapped");
-        },
-      ),
+	var listView = ListView.builder(
+			itemBuilder: (context, index) {
 
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
+				return ListTile(
+					leading: Icon(Icons.arrow_right),
+					title: Text(listItems[index]),
+					onTap: () {
+						debugPrint('${listItems[index]} was tapped');
+					},
+				);
+			}
+	);
 
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Phone"),
-      ),
-
-//      Text("Yet another element in List"),
-
-//      Container(color: Colors.red, height: 50.0,)
-
-    ],
-  );
-
-  return listView;
+	return listView;
 }
