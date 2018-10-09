9bd7d59e3d937e008ba5a2841867bbae45e03a5f
Smartherd <smartherd@gmail.com>
2018-10-09 11:58:16 +0530

    Section Three, Video Five: Creating UI of Simple Interest Calculater

--- lib/main.dart
+++ lib/main.dart
@@ -4,79 +4,146 @@ void main() {
 
 	runApp(
 		MaterialApp(
-			title: "Sateful App Example",
-			home: FavoriteCity(),
+			title: 'Simple Interest Calculator App',
+			home: SIForm(),
 		)
 	);
 }
 
-class FavoriteCity extends StatefulWidget {
+class SIForm extends StatefulWidget {
 
 	@override
   State<StatefulWidget> createState() {
-    return _FavoriteCityState();
+
+    return _SIFormState();
   }
 }
 
-class _FavoriteCityState extends State<FavoriteCity> {
+class _SIFormState extends State<SIForm> {
 
-	String nameCity = "";
-	var _currencies = ['Rupees', 'Dollars', 'Pounds', 'Others'];
-	var _currentItemSelected = 'Dollars';
+	var _currencies = ['Rupees', 'Dollars', 'Pounds'];
+	final double  _minimumPadding = 5.0;
 
 	@override
   Widget build(BuildContext context) {
 
-		debugPrint("Favorite City widget is created");
-
     return Scaffold(
+
 	    appBar: AppBar(
-		    title: Text("Stateful App Example"),
+		    title: Text('Simple Interest Calculator'),
 	    ),
+
 	    body: Container(
-		    margin: EdgeInsets.all(20.0),
+		    margin: EdgeInsets.all(_minimumPadding * 2),
 		    child: Column(
 			    children: <Widget>[
-			    	TextField(
-					    onSubmitted: (String userInput) {
-					    	setState(() {
-					    		debugPrint("set State is called, this tells framwork to redraw the FavCity widget");
-							    nameCity = userInput;
-					    	});
-					    },
-				    ),
-				    DropdownButton<String>(
 
-					    items: _currencies.map((String dropDownStringItem) {
-					    	return DropdownMenuItem<String>(
-							    value: dropDownStringItem,
-							    child: Text(dropDownStringItem),
-						    );
-					    }).toList(),
+				    getImageAsset(),
 
-					    onChanged: (String newValueSelected) {
-					    	// Your code to execute, when a menu item is selected from drop down
-						    _onDropDownItemSelected(newValueSelected);
-					    },
-
-					    value: _currentItemSelected,
-
-				    ),
 				    Padding(
-						    padding: EdgeInsets.all(30.0),
-						    child: Text(
-					        "Your best city is $nameCity",
-					        style: TextStyle(fontSize: 20.0),
-				    ))
+					    padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+					    child: TextField(
+					    keyboardType: TextInputType.number,
+					    decoration: InputDecoration(
+						    labelText: 'Principal',
+						    hintText: 'Enter Principal e.g. 12000',
+						    border: OutlineInputBorder(
+							    borderRadius: BorderRadius.circular(5.0)
+						    )
+					    ),
+				    )),
+
+				    Padding(
+					    padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+					    child: TextField(
+					    keyboardType: TextInputType.number,
+					    decoration: InputDecoration(
+							    labelText: 'Rate of Interest',
+							    hintText: 'In percent',
+							    border: OutlineInputBorder(
+									    borderRadius: BorderRadius.circular(5.0)
+							    )
+					    ),
+				    )),
+
+				    Padding(
+					    padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+					    child: Row(
+					    children: <Widget>[
+
+						    Expanded(child: TextField(
+							    keyboardType: TextInputType.number,
+							    decoration: InputDecoration(
+									    labelText: 'Term',
+									    hintText: 'Time in years',
+									    border: OutlineInputBorder(
+											    borderRadius: BorderRadius.circular(5.0)
+									    )
+							    ),
+						    )),
+
+						    Container(width: _minimumPadding * 5,),
+
+						    Expanded(child: DropdownButton<String>(
+							    items: _currencies.map((String value) {
+							    	return DropdownMenuItem<String>(
+									    value: value,
+									    child: Text(value),
+								    );
+							    }).toList(),
+
+							    value: 'Rupees',
+
+							    onChanged: (String newValueSelected) {
+							    	// Your code to execute, when a menu item is selected from dropdown
+							    },
+
+						    ))
+
+
+					    ],
+				    )),
+
+				    Padding(
+					    padding: EdgeInsets.only(bottom: _minimumPadding, top: _minimumPadding),
+					    child: Row(children: <Widget>[
+				    	Expanded(
+						    child: RaisedButton(
+							    child: Text('Calculate'),
+							    onPressed: () {
+
+							    },
+						    ),
+					    ),
+
+					    Expanded(
+						    child: RaisedButton(
+							    child: Text('Reset'),
+							    onPressed: () {
+
+							    },
+						    ),
+					    ),
+
+				    ],)),
+
+				    Padding(
+					    padding: EdgeInsets.all(_minimumPadding * 2),
+					    child: Text('Todo Text'),
+				    )
+
 			    ],
 		    ),
 	    ),
     );
   }
 
-  void _onDropDownItemSelected(String newValueSelected) {
-	  setState(() {
-		  this._currentItemSelected = newValueSelected;
-	  });
+  Widget getImageAsset() {
+
+		AssetImage assetImage = AssetImage('images/money.png');
+		Image image = Image(image: assetImage, width: 125.0, height: 125.0,);
+
+		return Container(child: image, margin: EdgeInsets.all(_minimumPadding * 10),);
   }
+
 }
