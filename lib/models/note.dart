07d2cc33103d7ce416b7830cc15c34f2096c1c28
Smartherd <smartherd@gmail.com>
2018-11-04 10:59:26 +0530

    Section Four, Video Six: Note Model class ( note.dart )

---
+++ lib/models/note.dart
@@ -0,0 +1,78 @@
+
+class Note {
+
+	int _id;
+	String _title;
+	String _description;
+	String _date;
+	int _priority;
+
+	Note(this._title, this._date, this._priority, [this._description]);
+
+	Note.withId(this._id, this._title, this._date, this._priority, [this._description]);
+
+	int get id => _id;
+
+	String get title => _title;
+
+	String get description => _description;
+
+	int get priority => _priority;
+
+	String get date => _date;
+
+	set title(String newTitle) {
+		if (newTitle.length <= 255) {
+			this._title = newTitle;
+		}
+	}
+
+	set description(String newDescription) {
+		if (newDescription.length <= 255) {
+			this._description = newDescription;
+		}
+	}
+
+	set priority(int newPriority) {
+		if (newPriority >= 1 && newPriority <= 2) {
+			this._priority = newPriority;
+		}
+	}
+
+	set date(String newDate) {
+		this._date = newDate;
+	}
+
+	// Convert a Note object into a Map object
+	Map<String, dynamic> toMap() {
+
+		var map = Map<String, dynamic>();
+		if (id != null) {
+			map['id'] = _id;
+		}
+		map['title'] = _title;
+		map['description'] = _description;
+		map['priority'] = _priority;
+		map['date'] = _date;
+
+		return map;
+	}
+
+	// Extract a Note object from a Map object
+	Note.fromMapObject(Map<String, dynamic> map) {
+		this._id = map['id'];
+		this._title = map['title'];
+		this._description = map['description'];
+		this._priority = map['priority'];
+		this._date = map['date'];
+	}
+}
+
+
+
+
+
+
+
+
+
