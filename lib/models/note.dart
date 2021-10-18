ad336125dc0cb84dac89c9ec92466bd03e97a966
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2021-10-19 00:13:15 +0530

    Adding null safety

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,78 +1,67 @@
-
 class Note {
+  int? _id;
+  String? _title;
+  String? _description;
+  String? _date;
+  int? _priority;
 
-	int _id;
-	String _title;
-	String _description;
-	String _date;
-	int _priority;
+  // default constructor
+  Note(this._title, this._date, this._priority, [this._description]);
 
-	Note(this._title, this._date, this._priority, [this._description]);
+  // Named constructor with ID
+  Note.withId(this._id, this._title, this._date, this._priority,
+      [this._description]);
 
-	Note.withId(this._id, this._title, this._date, this._priority, [this._description]);
+  // getters
+  int? get id => _id;
+  String? get title => _title;
+  String? get description => _description;
+  String? get date => _date;
+  int? get priority => _priority;
 
-	int get id => _id;
+  // setters
+  set title(String? newTitle) {
+    if (newTitle!.length <= 255) {
+      this._title = newTitle;
+    }
+  }
 
-	String get title => _title;
+  set description(String? newDescription) {
+    if (newDescription!.length <= 255) {
+      this._description = newDescription;
+    }
+  }
 
-	String get description => _description;
+  set priority(int? newPriority) {
+    if (newPriority! >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
 
-	int get priority => _priority;
+  set date(String? newDate) {
+    this._date = newDate;
+  }
 
-	String get date => _date;
+  // Convert a Note object into a map object
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['priority'] = _priority;
+    map['date'] = _date;
 
-	set title(String newTitle) {
-		if (newTitle.length <= 255) {
-			this._title = newTitle;
-		}
-	}
+    return map;
+  }
 
-	set description(String newDescription) {
-		if (newDescription.length <= 255) {
-			this._description = newDescription;
-		}
-	}
-
-	set priority(int newPriority) {
-		if (newPriority >= 1 && newPriority <= 2) {
-			this._priority = newPriority;
-		}
-	}
-
-	set date(String newDate) {
-		this._date = newDate;
-	}
-
-	// Convert a Note object into a Map object
-	Map<String, dynamic> toMap() {
-
-		var map = Map<String, dynamic>();
-		if (id != null) {
-			map['id'] = _id;
-		}
-		map['title'] = _title;
-		map['description'] = _description;
-		map['priority'] = _priority;
-		map['date'] = _date;
-
-		return map;
-	}
-
-	// Extract a Note object from a Map object
-	Note.fromMapObject(Map<String, dynamic> map) {
-		this._id = map['id'];
-		this._title = map['title'];
-		this._description = map['description'];
-		this._priority = map['priority'];
-		this._date = map['date'];
-	}
+  // Extract a note object from a map object
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this._id = map["id"];
+    this._title = map["title"];
+    this._description = map["description"];
+    this._priority = map["priority"];
+    this._date = map["date"];
+  }
 }
-
-
-
-
-
-
-
-
-
